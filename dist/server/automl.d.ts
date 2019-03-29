e2eb8bbecb29e56ad14f78c1a6a4a298514019d8
Lee Boonstra <lee@leeboonstra.com>
2019-03-29 11:07:16 +0100

    remove build files

--- dist/server/automl.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class AutoMl {
-    private sessionClient;
-    private projectId;
-    private location;
-    private model;
-    constructor();
-    detect(base64Img: string, callback: Function): void;
-}
-export declare let automl: AutoMl;
